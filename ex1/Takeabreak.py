80d311447d518405d430d2dff4df8680f2528086
ravibiz <rvbalichakra@yahoo.com>
2017-02-11 15:37:49 +0530

    commit from pycharm

---
+++ ex1/Takeabreak.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+total_breaks = 1
+break_count = 1
+
+print("Program is started on " + time.ctime())
+
+while (break_count <= total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=ib3r6mPD3aY") #play my favourite classical song by vidya and vandana from youtube
+    break_count = break_count + 1
+
+print("No more break!!")
